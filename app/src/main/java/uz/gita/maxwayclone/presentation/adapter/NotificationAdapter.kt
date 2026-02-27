c9ca0925233dd3b4bf483ab15525002919718436
Jasur653 <jasurtolqinov113@gmail.com>
2026-02-27 07:19:45 +0500

    load qoyildi

--- app/src/main/java/uz/gita/maxwayclone/presentation/adapter/NotificationAdapter.kt
+++ app/src/main/java/uz/gita/maxwayclone/presentation/adapter/NotificationAdapter.kt
@@ -1,11 +1,17 @@
 package uz.gita.maxwayclone.presentation.adapter
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import uz.gita.maxwayclone.databinding.ItomNotificationBinding
 import uz.gita.maxwayclone.domain.model.home.NotificationModel
 
@@ -31,9 +37,36 @@ class NotificationAdapter: ListAdapter<NotificationModel, NotificationAdapter.VH
                txtName.text = model.name
                 txtDescription.text = model.massage
                 txtDate.text = model.date
+                progress.visibility = View.VISIBLE
+
                 Glide.with(imgNotification.context)
                     .load(model.imgUrl)
+                    .centerCrop()
+                    .listener(object : RequestListener<Drawable>{
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable?>,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            progress.visibility = View.GONE
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable,
+                            model: Any,
+                            target: Target<Drawable?>?,
+                            dataSource: DataSource,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            progress.visibility = View.GONE
+                            return false
+                        }
+
+                    })
                     .into(imgNotification)
+
             }
 
 
